5828905e66cbb85fdb78b521a9f2d3ba0c569f0a
Uygarcan Boz <uygarcan.boz@gmail.com>
2023-12-25 08:44:13 +0300

    Matrix transpose calculator

---
+++ Matrix Transpose Calculator/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows in the matrix: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns in the matrix: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        // Input matrix elements
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Element at position (" + (i + 1) + "," + (j + 1) + "): ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Display the original matrix
+        System.out.println("\nOriginal Matrix:");
+        displayMatrix(matrix);
+
+        // Find and display the transpose of the matrix
+        int[][] transpose = findTranspose(matrix);
+        System.out.println("\nTranspose Matrix:");
+        displayMatrix(transpose);
+    }
+
+    private static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] findTranspose(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transpose = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        return transpose;
+    }
+}
